f2c802c4909ea33f83b8da6bd85dda33d55c0e95
kk2 <104248260+kk2a@users.noreply.github.com>
2023-12-26 20:29:09 +0900

    よく使うやつをメンバに入れた

--- UF-tree.cpp
+++ UF-tree.cpp
@@ -38,9 +38,10 @@ struct UnionFind_2D {
     using pi = pair<int, int>;
     vector<vector<int>> siz;
     vector<vector<pi>> parents;
+    int h_, w_;
 
     UnionFind_2D() {}
-    UnionFind_2D(int h, int w) {
+    UnionFind_2D(int h, int w) : h_(h), w_(w) {
         siz.resize(h, vector<int>(w, 0));
         parents.resize(h, vector<pi>(w, {0, 0}));
         for (int i = 0; i < h; i++) for (int j = 0; j < w; j++) {
@@ -76,6 +77,45 @@ struct UnionFind_2D {
     int treesize(pi x) {
         pi tmp = findroot(x);
         return siz[tmp.first][tmp.second];
+    }
+
+    void nice_unite(const vector<string>& s,
+                    const int& i, const int& j,
+                    const string& factor = "#") {
+        int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+        for (int k = 0; k < 4; k++) {
+            int nx = i + dx[k], ny = j + dy[k];
+            if (!(0 <= nx && nx < h_ && 0 <= ny && ny < w_)) continue;
+            if (factor.find(s[nx][ny]) == string::npos) continue;
+            unite({i, j}, {nx, ny});
+        }
+    }
+
+    int count_arround(const vector<string>& s,
+                      const int& i, const int& j,
+                      const string& factor = "#") {
+        int dx[] = {1, 0, -1, 0}, dy[] = {0, 1, 0, -1};
+        set<pi> res;
+        for (int k = 0; k < 4; k++) {
+            int nx = i + dx[k], ny = j + dy[k];
+            if (!(0 <= nx && nx < h_ && 0 <= ny && ny < w_)) continue;
+            if (factor.find(s[nx][ny]) == string::npos) continue;
+            res.insert(findroot({nx, ny}));
+        }
+        return int(size(res));
+    }
+
+    // count connected component
+    int cococo(const vector<string>& s, const string& factor = "#") {
+        vector<vector<bool>> count(h_, vector<bool>(w_, false));
+        int res = 0;
+        for (int i = 0; i < h_; i++) for (int j = 0; j < w_; j++) {
+            if (factor.find(s[i][j]) == string::npos) continue;
+            pi tmp = findroot({i, j});
+            if (!count[tmp.first][tmp.second]) res++;
+            count[tmp.first][tmp.second] = true;
+        }
+        return res;
     }
 };
 
