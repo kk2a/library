cb9bd30251ef8f2061a3c8a81abe1f7f0864a194
kk2 <104248260+kk2a@users.noreply.github.com>
2023-12-28 17:59:04 +0900

    重み付きunionfindの実装

--- UF-tree.cpp
+++ UF-tree.cpp
@@ -48,6 +48,43 @@ struct UnionFind_rollback {
     }
 };
 
+// A is an abelian group
+template <class A, A (*op)(A, A), A (*e)(), A (*inv)(A)>
+struct UnionFind_weight {
+  private:
+    vector<int> d;
+    vector<A> diff_weight;
+  public:
+    UnionFind_weight(int n = 0) : d(n, -1), diff_weight(n, e()) {}
+    bool unite(int x, int y, A w) {
+        w = op(w, op(weight(x), inv(weight(y))));
+        x = find(x); y = find(y);
+        if (x == y) return false;
+        if (d[x] > d[y]) { swap(x, y); w = inv(w); }
+        d[x] += d[y];
+        d[y] = x;
+        diff_weight[y] = w;
+        return true;
+    }
+    int find(int x) {
+        if (d[x] < 0) return x;
+        int r = find(d[x]);
+        diff_weight[x] = op(diff_weight[x], diff_weight[d[x]]);
+        return d[x] = r;
+    }
+    bool same(int x, int y) { return find(x) == find(y); }
+    int size(int x) { return -d[find(x)]; }
+
+    A weight(int x) {
+        find(x);
+        return diff_weight[x];
+    }
+    A diff(int a, int b) {
+        return op(inv(weight(a)), weight(b));
+    }
+};
+
+
 int main() {
     
     return 0;
