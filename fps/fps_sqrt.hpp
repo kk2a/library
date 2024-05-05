a042876e85257736985db6d73297b66930e08f48
kk2 <104248260+kk2a@users.noreply.github.com>
2024-05-06 08:24:22 +0900

    i came

--- fps/fps_sqrt.hpp
+++ fps/fps_sqrt.hpp
@@ -38,25 +38,25 @@ FormalPowerSeries<mint> sqrt(FormalPowerSeries<mint> f, int deg = -1) {
 template <class mint>
 FormalPowerSeries<mint> sparse_sqrt(const FormalPowerSeries<mint>& f,
                                     int deg = -1) {
-        if (deg == -1) deg = (int)f.size();
-        if ((int)f.size() == 0) return FormalPowerSeries<mint>(deg, mint(0));
-        if (f[0] == mint(0)) {
-            for (int i = 1; i < (int)f.size(); i++) {
-                if (f[i] != mint(0)) {
-                    if (i & 1) return {};
-                    if (deg - i / 2 <= 0) break;
-                    auto ret = sparse_sqrt(f >> i, deg - i / 2);
-                    if (ret.empty()) return {};
-                    ret = ret << (i / 2);
-                    if ((int)ret.size() < deg) ret.resize(deg, mint(0));
-                    return ret;
-                }
+    if (deg == -1) deg = (int)f.size();
+    if ((int)f.size() == 0) return FormalPowerSeries<mint>(deg, mint(0));
+    if (f[0] == mint(0)) {
+        for (int i = 1; i < (int)f.size(); i++) {
+            if (f[i] != mint(0)) {
+                if (i & 1) return {};
+                if (deg - i / 2 <= 0) break;
+                auto ret = sparse_sqrt(f >> i, deg - i / 2);
+                if (ret.empty()) return {};
+                ret = ret << (i / 2);
+                if ((int)ret.size() < deg) ret.resize(deg, mint(0));
+                return ret;
             }
-            return FormalPowerSeries<mint>(deg, mint(0));
         }
-        long long sqr = mod_sqrt(f[0].val(), mint::getmod());
-        if (sqr == -1) return {};
-        return f.sparse_pow(((mint::getmod() + 1) >> 1), deg) * mint(sqr).inv();
+        return FormalPowerSeries<mint>(deg, mint(0));
+    }
+    long long sqr = mod_sqrt(f[0].val(), mint::getmod());
+    if (sqr == -1) return {};
+    return f.sparse_pow(((mint::getmod() + 1) >> 1), deg) * mint(sqr).inv();
 }
 
 #endif // FPS_SQRT_HPP
