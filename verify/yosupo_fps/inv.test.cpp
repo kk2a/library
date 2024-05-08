040162fd274b87c10d59acb1ef76e84d32f0ad03
kk2 <104248260+kk2a@users.noreply.github.com>
2024-05-08 19:48:38 +0900

    no eat

--- verify/yosupo_fps/inv.test.cpp
+++ verify/yosupo_fps/inv.test.cpp
@@ -1,6 +1,6 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/inv_of_formal_power_series"
 
-#include <../../template/template.hpp>
+#include <"../../template/template.hpp">
 #include <"../../modint/mont.hpp">
 #include <"../../fps/fps.hpp">
 #include <"../../convolution/butterfly.hpp">
