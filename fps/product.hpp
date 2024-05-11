2fc9c8754290129c58b62847c8329cdbb9abe330
kk2 <104248260+kk2a@users.noreply.github.com>
2024-05-11 18:04:27 +0900

    template

--- fps/product.hpp
+++ fps/product.hpp
@@ -1,11 +1,10 @@
 #ifndef FPS_PRODUCT_HPP
 #define FPS_PRODUCT_HPP 1
 
-#include "fps.hpp"
 
-template <class mint>
-FormalPowerSeries<mint> all_prod(const vector<FormalPowerSeries<mint>> &a, int l, int r) {
-    if (l == r) return FormalPowerSeries<mint>{1};
+template <class mint, class FPS>
+FormalPowerSeries<mint> all_prod(const vector<FPS> &a, int l, int r) {
+    if (l == r) return FPS{1};
     if (l + 1 == r) return a[l];
     int m = (l + r) >> 1;
     return all_prod(a, l, m) * all_prod(a, m, r);
