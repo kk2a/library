421590413829281580a67b12ce8003627af1fa73
kk2 <104248260+kk2a@users.noreply.github.com>
2024-05-18 09:36:10 +0900

    matrix field

--- matrix/matrix_field.hpp
+++ matrix/matrix_field.hpp
@@ -2,12 +2,12 @@
 #define MATRIX_HPP 1
 
 template <class Field>
-struct matrix {
-    using mat = matrix;
-    matrix() : matrix(0) {}
-    matrix(int n) : matrix(n, n) {}
-    matrix(int h, int w) : matrix(vector<vector<Field>>(h, vector<Field>(w, Field()))) {}
-    matrix(const vector<vector<Field>>& mat) : _h(mat.size()), _w(mat[0].size()), mat_(mat) {}
+struct MatrixField {
+    using mat = MatrixField;
+    MatrixField() : MatrixField(0) {}
+    MatrixField(int n) : MatrixField(n, n) {}
+    MatrixField(int h, int w) : MatrixField(vector<vector<Field>>(h, vector<Field>(w, Field()))) {}
+    MatrixField(const vector<vector<Field>>& mat) : _h(mat.size()), _w(mat[0].size()), mat_(mat) {}
     
     int get_h() const { return _h; }
     int get_w() const { return _w; }
@@ -16,6 +16,15 @@ struct matrix {
         assert(0 <= i && i < _h);
         assert(0 <= j && j < _w);
         return mat_[i][j];
+    }
+
+    vector<vector<Field>> get_mat() const {
+        return mat_;
+    }
+
+    vector<Field>& operator[](int i) const {
+        assert(0 <= i && i < _h);
+        return mat_[i];
     }
 
     void display() const {
@@ -66,7 +75,7 @@ struct matrix {
         return *this;
     }
 
-    Field det(Field product_e) const {
+    Field det(Field product_e = Field(1)) const {
         assert(_h == _w);
         int n = _h;
         vector<vector<Field>> a(mat_);
@@ -79,7 +88,7 @@ struct matrix {
                     break;
                 }
             }
-            if (pivot == -1) return Field();
+            if (pivot == -1) return Field(0);
             if (i != pivot) {
                 swap(a[i], a[pivot]);
                 res = -res;
@@ -100,7 +109,7 @@ struct matrix {
         return res;
     }
 
-    mat inv(Field product_e) const {
+    mat inv(Field product_e = Field(1)) const {
         assert(_h == _w);
         int n = _h;
         vector<vector<Field>> res(n, vector<Field>(n, Field()));
@@ -147,6 +156,95 @@ struct matrix {
             x *= x;
         }
         return res;
+    }
+
+    int rank(Field product_e = Field(1)) const {
+        int n = _h, m = _w;
+        vector<vector<Field>> a(mat_);
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            int pivot = -1;
+            for (int j = res; j < n; j++) {
+                if (a[j][i] != 0) {
+                    pivot = j;
+                    break;
+                }
+            }
+            if (pivot == -1) continue;
+            swap(a[res], a[pivot]);
+            Field inv = product_e / a[res][i];
+            for (int j = i; j < m; j++) {
+                a[res][j] *= inv;
+            }
+            for (int j = res + 1; j < n; j++) {
+                Field r = a[j][i];
+                for (int k = i; k < m; k++) {
+                    a[j][k] -= a[res][k] * r;
+                }
+            }
+            res++;
+        }
+        return res;
+    }
+
+    mat& inplace_combine_top(const mat& rhs) {
+        assert(_w == rhs._w);
+        this->mat_.insert(begin(this->mat_),
+            begin(rhs.mat_), end(rhs.mat_));
+        this->_h += rhs._h;
+        return *this;
+    }
+
+    mat combine_top(const mat& rhs) const {
+        assert(_w == rhs._w);
+        mat res(this->mat_);
+        return res.inplace_combine_top(rhs);
+    }
+
+    mat& inplace_combine_bottom(const mat& rhs) {
+        assert(_w == rhs._w);
+        this->mat_.insert(end(this->mat_),
+            begin(rhs.mat_), end(rhs.mat_));
+        this->_h += rhs._h;
+        return *this;
+    }
+
+    mat combine_bottom(const mat& rhs) const {
+        assert(_w == rhs._w);
+        mat res(this->mat_);
+        return res.inplace_combine_bottom(rhs);
+    }
+
+    mat& inplace_combine_left(const mat& rhs) {
+        assert(_h == rhs._h);
+        for (int i = 0; i < _h; i++) {
+            this->mat_[i].insert(begin(this->mat_[i]),
+                begin(rhs.mat_[i]), end(rhs.mat_[i]));
+        }
+        this->_w += rhs._w;
+        return *this;
+    }
+
+    mat combine_left(const mat& rhs) const {
+        assert(_h == rhs._h);
+        mat res(this->mat_);
+        return res.inplace_combine_left(rhs);
+    }
+
+    mat& inplace_combine_right(const mat& rhs) {
+        assert(_h == rhs._h);
+        for (int i = 0; i < _h; i++) {
+            this->mat_[i].insert(end(this->mat_[i]),
+                begin(rhs.mat_[i]), end(rhs.mat_[i]));
+        }
+        this->_w += rhs._w;
+        return *this;
+    }
+
+    mat combine_right(const mat& rhs) const {
+        assert(_h == rhs._h);
+        mat res(this->mat_);
+        return res.inplace_combine_right(rhs);
     }
 
     friend mat operator+(const mat& lhs, const mat& rhs) {
