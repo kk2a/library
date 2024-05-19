753587f82969056ef2391a7ea5d01c474766204e
kk2 <104248260+kk2a@users.noreply.github.com>
2024-05-19 15:28:19 +0900

    templateに対応

--- kyopro.py
+++ kyopro.py
@@ -15,41 +15,41 @@ class include_file:
         self.graph = []
 
     def query(self):
-        with open(self.file_path, 'r') as file:
-            for line in file:
-                if not line.startswith('#include'):
-                    continue
-                included_file = line.split()[1].strip('"<>\n')
-                if not included_file.startswith('kk2'):
-                    continue
-                included_file = good_path(os.path.join(self.include_path, included_file))
-                if included_file in self.id.keys():
-                    continue
-                self.id[included_file] = len(self.id)
-                self.iid.append(included_file)
-                self.graph.append([])
-                self.rec(included_file)
-        self.sorted = []
-        self.toposo()
+        self.rec(self.file_path)
+        self.sorted = self.toposo()
         self.erase_incude_file()
         self.write_include_file()
 
     def rec(self, cur):
-        print(cur)
+        # print(cur)
+        flag = self.file_path == cur
         with open(cur, 'r') as file:
             for line in file:
                 if not line.startswith('#include'):
                     continue
-                if line.endswith('>\n'):
-                    continue
-                included_file = line.split()[1].strip('"\n')
-                included_file = good_path(os.path.join(os.path.dirname(cur), included_file))
+
+                if flag:
+                    included_file = line.split()[1].strip('"<>\n')
+                    if not included_file.startswith('kk2'):
+                        continue
+                else:
+                    if line.endswith('>\n'):
+                        continue
+                    included_file = line.split()[1].strip('"\n')
+
+                if flag:
+                    included_file = good_path(os.path.join(self.include_path, included_file))
+                else:
+                    included_file = good_path(os.path.join(os.path.dirname(cur), included_file))
+
                 if included_file in self.id.keys():
-                    self.graph[self.id[cur]].append(self.id[included_file])
+                    if not flag:
+                        self.graph[self.id[cur]].append(self.id[included_file])
                     continue
                 self.id[included_file] = len(self.id)
                 self.iid.append(included_file)
-                self.graph[self.id[cur]].append(self.id[included_file])
+                if not flag:
+                    self.graph[self.id[cur]].append(self.id[included_file])
                 self.graph.append([])
                 self.rec(included_file)
 
@@ -68,7 +68,7 @@ class include_file:
             if not visited[i]:
                 dfs(i)
 
-        self.sorted = stack
+        return stack
 
     def erase_incude_file(self):
         with open(self.file_path, 'r') as file:
@@ -82,15 +82,10 @@ class include_file:
         with open(self.file_path, 'r') as file:
             lines = file.readlines()
         with open(self.file_path, 'w') as file:
-            i = -1
-            for i, line in enumerate(lines):
-                if line.rstrip() == 'using namespace std;':
-                    i += 1
-                    break
             tmp = self.all_include_files()
             for line in reversed(tmp):
                 # print(len(tmp))
-                lines.insert(i, line)
+                lines.insert(0, line)
             # lines.insert(i, self.all_include_files())
             for line in lines:
                 file.write(line)
@@ -101,12 +96,12 @@ class include_file:
             with open(self.iid[i], 'r') as file:
                 lines = file.readlines()
                 for line in lines:
-                    if line.startswith('#include'):
+                    if (line.startswith('#include') and
+                        not line.endswith('>\n')):
                         continue
                     # print(line)
                     ret.append(line)
         return ret
-
 
 
 def main(*args):
