99ec219e11aca7005c7c7eca5618433a5fd267f9
kk2 <104248260+kk2a@users.noreply.github.com>
2024-06-13 16:56:55 +0900

    graph

--- graph/topological_sort.hpp
+++ graph/topological_sort.hpp
@@ -20,4 +20,23 @@ vector<int> topological_sort(const vector<vector<int>>& graph) {
 	return res;
 }
 
+template <class G>
+vector<int> topological_sort(const G& graph) {
+	int siz = size(graph);
+	vector<int> indegree(siz);
+	for (int i = 0; i < siz; i++) for (int j : graph[i]) indegree[j]++;
+	vector<int> res;
+	queue<int> que;
+	for (int i = 0; i < siz; i++) if (indegree[i] == 0) que.push(i);
+	while (!que.empty()) {
+		int ver = que.front(); que.pop();
+		res.push_back(ver);
+		for (int i : graph[ver]) {
+			indegree[i]--;
+			if (indegree[i] == 0) que.push(i);
+		}
+	}
+	return res;
+}
+
 #endif // TOPOLOGICAL_SORT_HPP
