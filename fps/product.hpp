2868d30d32f1df58235b4f2e8836bf84e1031b03
kk2 <104248260+kk2a@users.noreply.github.com>
2024-06-16 14:35:45 +0900

    nice Args

--- fps/product.hpp
+++ fps/product.hpp
@@ -3,11 +3,16 @@
 
 
 template <class FPS, class mint = typename FPS::value_type>
-FormalPowerSeries<mint> all_prod(const vector<FPS> &a, int l, int r) {
+FPS all_prod(const vector<FPS> &a) {
+    return inner_all_prod(a, 0, (int)a.size());
+}
+
+template <class FPS, class mint = typename FPS::value_type>
+FPS inner_all_prod(const vector<FPS> &a, int l, int r) {
     if (l == r) return FPS{1};
     if (l + 1 == r) return a[l];
     int m = (l + r) >> 1;
-    return all_prod(a, l, m) * all_prod(a, m, r);
+    return inner_all_prod(a, l, m) * inner_all_prod(a, m, r);
 }
 
 #endif // FPS_PRODUCT_HPP
