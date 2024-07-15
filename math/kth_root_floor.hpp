379beaafe4384ce527d58a322b8deeccde3287fa
kk2 <104248260+kk2a@users.noreply.github.com>
2024-07-15 23:05:18 +0900

    floor

---
+++ math/kth_root_floor.hpp
@@ -0,0 +1,46 @@
+#ifndef MATH_KTH_ROOT_FLOOR_HPP
+#define MATH_KTH_ROOT_FLOOR_HPP 1
+
+uint64_t kth_root_floor_inner(uint64_t a, int k) {
+    if (a <= 1 || k == 1) return a;
+    if (64 <= k) return 1;
+    auto check = [&](__uint128_t x) {
+        __uint128_t p = 1, q = x;
+        for (int b = k; b; b >>= 1, q *= q) {
+            if (b & 1) p *= q;
+        }
+        return p <= a;
+    };
+    uint64_t x = powl(a, (long double)1.0 / k);
+    while (!check(x)) --x;
+    while (check(x + 1)) ++x;
+    return x;
+}
+
+// return floor(a ^ {1/k})
+template <class return_type=uint64_t, class T, class U>
+return_type kth_root_floor(T a, U k) {
+    return (return_type)kth_root_floor_inner((uint64_t)a, (int)k);
+}
+
+uint64_t kth_root_ceil_inner(uint64_t a, int k) {
+    if (a <= 1 || k == 1) return a;
+    if (64 <= k) return 1;
+    auto check = [&](__uint128_t x) {
+        __uint128_t p = 1, q = x;
+        for (int p = k; p; p >>= 1, q *= q) {
+            if (p & 1) p *= x;
+        }
+        return q == a;
+    };
+    uint64_t x = kth_root_floor_inner(a, k);
+    return check(x) ? x : x + 1;
+}
+
+// return ceil(a ^ {1/k})
+template <class return_type=uint64_t, class T, class U>
+return_type kth_root_ceil(T a, U k) {
+    return (return_type)kth_root_ceil_inner((uint64_t)a, (int)k);
+}
+
+#endif // MATH_KTH_ROOT_FLOOR_HPP
