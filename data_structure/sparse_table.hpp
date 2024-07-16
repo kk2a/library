ea59df8c798760c9a95c04682095f12e7618307a
kk2 <104248260+kk2a@users.noreply.github.com>
2024-07-17 02:28:55 +0900

    add max_right min_left

--- data_structure/sparse_table.hpp
+++ data_structure/sparse_table.hpp
@@ -23,11 +23,53 @@ struct SparseTable {
     static S Op(S l, S r) { return op(l, r); }
     static S MonoidUnit() { return e(); }
 
-    S prod(int l, int r) {
+    S prod(int l, int r) const {
         assert(0 <= l && l <= r && r <= _n);
         if (l == r) return e();
         int i = 31 - __builtin_clz(r - l);
         return op(table[i][l], table[i][r - (1 << i)]);
+    }
+
+    // return r s.t.
+    // r = l or f(op(a[l], a[l+1], ..., a[r-1])) == true
+    // r = n or f(op(a[l], a[l+1], ..., a[r]))   == false
+    template <bool (*f)(S)>
+    int max_right(int l) const {
+        return max_right(l, [](S x) { return f(x); });
+    }
+    template <class F>
+    int max_right(int l, F f) const {
+        assert(0 <= l && l <= _n);
+        assert(f(e()));
+        if (l == _n) return _n;
+        int left = l - 1, right = _n;
+        while (right - left > 1) {
+            int mid = (left + right) >> 1;
+            if (f(prod(l, mid))) left = mid;
+            else right = mid;
+        }
+        return right;
+    }
+
+    // return l s.t.
+    // l = r or f(op(a[l], a[l+1], ..., a[r-1])) == false
+    // l = 0 or f(op(a[l], a[l+1], ..., a[r]))   == true
+    template <bool (*f)(S)>
+    int min_left(int r) const {
+        return min_left(r, [](S x) { return f(x); });
+    }
+    template <class F>
+    int min_left(int r, F f) const {
+        assert(0 <= r && r <= _n);
+        assert(f(e()));
+        if (r == 0) return 0;
+        int left = -1, right = r;
+        while (right - left > 1) {
+            int mid = (left + right) >> 1;
+            if (f(prod(mid, r))) right = mid;
+            else left = mid;
+        }
+        return right;
     }
 
   private:
