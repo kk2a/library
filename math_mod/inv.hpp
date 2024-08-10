3f6893ab71a076a0a110f5b1d65c2b6d177812a2
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-10 15:51:37 +0900

    reduce template args

--- math_mod/inv.hpp
+++ math_mod/inv.hpp
@@ -1,16 +1,20 @@
 #ifndef MOD_INV_HPP
 #define MOD_INV_HPP 1
 
-template <class T, class U>
-constexpr long long mod_inversion(T a, U modulo) {
-    long long s = modulo, t = a;
-    long long m0 = 0, m1 = 1;
+
+// require: modulo >= 1
+template <class T>
+constexpr T mod_inversion(T a, T modulo) {
+    a %= modulo;
+    if (a < 0) a += modulo;
+    T s = modulo, t = a;
+    T m0 = 0, m1 = 1;
     while (t) {
-        long long u = s / t;
+        T u = s / t;
         swap(s -= t * u, t);
         swap(m0 -= m1 * u, m1);
     }
-    if (m0 < 0) m0 += modulo / s;
+    if (m0 < 0) m0 += modulo;
     return m0;
 }
 
