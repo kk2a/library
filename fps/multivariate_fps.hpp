8accecbe59236e639da0be4d8e808803ea0b6d0b
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-13 11:48:13 +0900

    multi fps

---
+++ fps/multivariate_fps.hpp
@@ -0,0 +1,12 @@
+#ifndef FPS_MULTIVARIATE_FPS_HPP
+#define FPS_MULTIVARIATE_FPS_HPP 1
+
+#include "fps.hpp"
+
+template <typename mint>
+sturct MultivariateFormalPowerSeries {
+    using mgps = MultivariateFormalPowerSeries;
+    using fps = FormalPowerSeries<mint>;
+};
+
+#endif // FPS_MULTIVARIATE_FPS_HPP
