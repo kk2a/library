e1c3d289b8262e47e4ea7c833f24861643a83b21
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-19 03:41:55 +0900

    return *this

--- string/rolling_hash.hpp
+++ string/rolling_hash.hpp
@@ -3,9 +3,8 @@
 
 #include "../math_mod/pow_expr.hpp"
 
-// setbase() must be called before using Hashs
-template <int id>
-struct Hashs {
+// setbase() must be called before using Hash
+struct RollingHash {
     struct Hash {
         long long h, pw, pwi;
     };
@@ -18,14 +17,13 @@ struct Hashs {
     static void setbase() {
         mt19937_64 rng(time(0));
         for (int i = 0; i < b; ++i) {
-            unordered_set<int> st = {0, 1, modp[i] - 1};
-            while (st.count(base[i] = rng() % modp[i]));
+            base[i] = rng() % modp[i];
             basei[i] = quo(base[i], i);
         }
     }
 
     template <class T>
-    Hashs(const T &v) {
+    RollingHash(const T &v) {
         for (int i = 0; i < b; ++i) {
             table[i].h = v % modp[i];
             table[i].pw = base[i];
@@ -34,7 +32,7 @@ struct Hashs {
     }
 
     template <>
-    Hashs(char c) {
+    RollingHash(char c) {
         for (int i = 0; i < b; ++i) {
             table[i].h = c;
             table[i].pw = base[i];
@@ -43,7 +41,7 @@ struct Hashs {
     }
 
     template <class T>
-    Hashs(const vector<T> &v) {
+    RollingHash(const vector<T> &v) {
         int n = v.size();
         for (int i = 0; i < b; ++i) {
             table[i].h = 0;
@@ -58,7 +56,7 @@ struct Hashs {
     }
 
     template <>
-    Hashs(const string &v) {
+    RollingHash(const string &v) {
         int n = s.size();
         for (int i = 0; i < b; ++i) {
             table[i].h = 0;
@@ -72,7 +70,7 @@ struct Hashs {
         }
     }
 
-    Hashs() {
+    RollingHash() {
         for (int i = 0; i < b; ++i) {
             table[i].h = 0;
             table[i].pw = 1;
@@ -80,32 +78,35 @@ struct Hashs {
         }
     }
 
-    void push_front(const Hashs &otherHash) {
+    RollingHash& push_front(const RollingHash &otherHash) {
         for (int i = 0; i < b; ++i) {
             table[i].h = (otherHash.table[i].h + table[i].h * otherHash.table[i].pw) % modp[i];
             table[i].pw = table[i].pw * otherHash.table[i].pw % modp[i];
             table[i].pwi = table[i].pwi * otherHash.table[i].pwi % modp[i];
         }
+        return *this;
     }
 
-    void push_back(const Hashs &otherHash) {
+    RollingHash& push_back(const RollingHash &otherHash) {
         for (int i = 0; i < b; ++i) {
             table[i].h = (table[i].h + otherHash.table[i].h * table[i].pw) % modp[i];
             table[i].pw = table[i].pw * otherHash.table[i].pw % modp[i];
             table[i].pwi = table[i].pwi * otherHash.table[i].pwi % modp[i];
         }
+        return *this;
     }
 
-    void pop_front(const Hashs &otherHash) {
+    RollingHash& pop_front(const RollingHash &otherHash) {
         for (int i = 0; i < b; ++i) {
             table[i].h = (table[i].h - otherHash.table[i].h) * otherHash.table[i].pwi % modp[i];
             if (table[i].h < 0) table[i].h += modp[i];
             table[i].pw = table[i].pw * otherHash.table[i].pwi % modp[i];
             table[i].pwi = table[i].pwi * otherHash.table[i].pw % modp[i];
         }
+        return *this;
     }
 
-    void pop_back(const Hashs &otherHash) {
+    RollingHash& pop_back(const RollingHash &otherHash) {
         for (int i = 0; i < b; ++i) {
             long long minus = otherHash.table[i].h * table[i].pw % modp[i] * otherHash.table[i].pwi % modp[i];
             table[i].h = (table[i].h - minus) % modp[i];
@@ -113,16 +114,17 @@ struct Hashs {
             table[i].pw = table[i].pw * otherHash.table[i].pwi % modp[i];
             table[i].pwi = table[i].pwi * otherHash.table[i].pw % modp[i];
         }
+        return *this;
     }
 
-    friend bool operator==(const Hashs &lhs, const Hashs &rhs) {
+    friend bool operator==(const RollingHash &lhs, const RollingHash &rhs) {
         for (int i = 0; i < b; ++i) {
             if (lhs.table[i].h != rhs.table[i].h) return false;
             if (lhs.table[i].pw != rhs.table[i].pw) return false;
         }
         return true;
     }
-    friend bool operator!=(const Hashs &lhs, const Hashs &rhs) {
+    friend bool operator!=(const RollingHash &lhs, const RollingHash &rhs) {
         return !(lhs == rhs);
     }
 
@@ -132,11 +134,14 @@ struct Hashs {
     } 
 };
 
-template <int id>
-int Hashs<id>::base[5] = {3, 3, 3, 3, 3};
-template <int id>
-int Hashs<id>::basei[5] = {332748118, 333333336, 666666673, 666666681, 666666689};
+int RollingHash::base[5] = {3, 3, 3, 3, 3};
+int RollingHash::basei[5] = {332748118, 333333336, 666666673, 666666681, 666666689};
 
-using Roliha = Hashs<0>;
+namespace kk2 {
+
+using Roliha = RollingHash;
+
+} // namespace kk2
+
 
 #endif // ROLLING_HASH_HPP
