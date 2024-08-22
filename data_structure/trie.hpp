dc214cc552b3b6b90c1c26589f81ecf44e1839f6
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-22 19:31:27 +0900

    add trie

---
+++ data_structure/trie.hpp
@@ -0,0 +1,73 @@
+#ifndef DATA_STRUCTURE_TRIE_HPP
+#define DATA_STRUCTURE_TRIE_HPP 1
+
+template <int char_size>
+struct TrieNode {
+    int nxt[char_size];
+    int exist;
+    vector<int> accept;
+    TrieNode() : exist(0) { memset(nxt, -1, sizeof(nxt)); }
+};
+
+template <int char_size, int margin>
+struct Trie {
+    using Node = TrieNode<char_size>;
+
+    vector<Node> nodes;
+    int root;
+
+    Trie() : root(0) { nodes.emplace_back(); }
+
+    int push_node() {
+        nodes.emplace_back();
+        return (int)nodes.size() - 1;
+    }
+
+    void update_direct(int node, int id) { nodes[node].accept.push_back(id); }
+
+    void update_child(int node) { ++nodes[node].exist; }
+
+    void add(const string& str) {
+        const int id = nodes[root].exist;
+        auto rec = [&](auto self, int now, int idx) -> void {
+            if (idx == (int)str.size()) {
+                update_direct(now, id);
+                return;
+            }
+            const int d = str[idx] - margin;
+            if (nodes[now].nxt[d] == -1) nodes[now].nxt[d] = push_node();
+            self(self, nodes[now].nxt[d], idx + 1);
+            update_child(now);
+        };
+        rec(rec, root, 0);
+    }
+
+    template <void (*f)(int)>
+    void query(const string& str) {
+        query(str, [](int idx) { f(idx); });
+    }
+
+    template <class F>
+    void query(const string& str, const F& f) {
+        int now = root;
+        for (char c : str) {
+            for (int& idx : nodes[now].accept) f(idx);
+            const int d = c - margin;
+            now = nodes[now].nxt[d];
+            if (now == -1) return;
+        }
+        for (int idx : nodes[now].accept) f(idx);
+    }
+
+    int count() const { return (int)nodes[0].exist; }
+
+    int size() const { return (int)nodes.size(); }
+
+    // return the number of strings which have the prefix
+    // responding to the node_id
+    int size(int node_idx) const {
+        return (int)nodes[node_idx].accept.size() + nodes[node_idx].exist;
+    }
+};
+
+#endif // DATA_STRUCTURE_TRIE_HPP
