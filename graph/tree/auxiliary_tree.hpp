a94a12591b5fd461e580cd50a7217d852825b596
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-23 20:53:53 +0900

    add aux tree

--- graph/tree/auxiliary_tree.hpp
+++ graph/tree/auxiliary_tree.hpp
@@ -1,5 +1,36 @@
 #ifndef GRAPH_TREE_AUXILIARY_TREE_HPP
 #define GRAPH_TREE_AUXILIARY_TREE_HPP 1
 
+#include "heavy_light_decomposition.hpp"
+
+template <typename G>
+struct AuxiliaryTree {
+    G g;
+    HeavyLightDecomposition<G> hld;
+    AuxiliaryTree(const G& g_, int root_ = 0) : g(g_), hld(g, root_) {}
+
+    pair<vector<vector<int>>, vector<int>> get(vector<int> ps) {
+        if (ps.empty()) return {};
+        sort(begin(ps), end(ps), [&](int i, int j){
+            return hld.in[i] < hld.in[j]; });
+        for (int i = 0, ps_size = ps.size(); i < ps_size - 1; i++) {
+            ps.push_back(hld.lca(ps[i], ps[i + 1]));
+        }
+        sort(begin(ps), end(ps), [&](int i, int j){
+            return hld.in[i] < hld.in[j]; });
+        ps.erase(unique(begin(ps), end(ps)), end(ps));
+
+        vector<vector<int>> aux(ps.size());
+        stack<int> st;
+        st.emplace(0);
+        for (int i = 1; i < (int)ps.size(); i++) {
+            int l = hld.lca(ps[i], ps[st.top()]);
+            while (ps[st.top()] != l) st.pop();
+            aux[st.top()].emplace_back(i);
+            st.emplace(i);
+        }
+        return make_pair(aux, ps);
+    }
+}; 
 
 #endif // GRAPH_TREE_AUXILIARY_TREE_HPP
