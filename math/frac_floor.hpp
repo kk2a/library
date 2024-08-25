2d998ff2741ffba63431b04a3867e994683bc689
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-26 04:45:13 +0900

    debi debi debiru

--- math/frac_floor.hpp
+++ math/frac_floor.hpp
@@ -1,8 +1,8 @@
 #ifndef MATH_FLOOR_CEIL_HPP
 #define MATH_FLOOR_CEIL_HPP 1
 
-// if x \notin Z, then floor(x) = ceil(x) - 1 ...(1)
-// for all x \in R, floor(x) = -ceil(-x)      ...(2)
+// floor(x) = ceil(x) - 1 (for all x not in Z) ...(1)
+// floor(x) = -ceil(-x)   (for all x)          ...(2)
 
 // return floor(a / b)
 template <typename T, typename U>
@@ -11,7 +11,6 @@ T fracfloor(T a, U b) {
     if (a % b == 0) return a / b;
     if (a >= 0) return a / b;
 
-    // if x \notin Z,
     // floor(x) = -ceil(-x)      by (2)
     //          = -floor(-x) - 1 by (1)
     return -((-a) / b) - 1;
@@ -24,7 +23,6 @@ T fracceil(T a, U b) {
     if (a % b == 0) return a / b;
     if (a >= 0) return a / b + 1;
 
-    // if x \notin Z,
     // ceil(x) = -floor(-x)      by (2)
     return -((-a) / b);
 }
