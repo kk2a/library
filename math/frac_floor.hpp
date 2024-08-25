9c7d647dcc319d99aad396e7959d80c8d8c75233
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-26 01:49:22 +0900

    template

--- math/frac_floor.hpp
+++ math/frac_floor.hpp
@@ -1,16 +1,18 @@
 #ifndef MATH_FLOOR_CEIL_HPP
 #define MATH_FLOOR_CEIL_HPP 1
 
-// long long floor
-long long fracfloor(long long a, long long b) {
+// return floor(a / b)
+template <typename T, typename U, typename R = T>
+R fracfloor(T a, U b) {
     assert(b != 0);
     if (a % b == 0) return a / b;
     if (a >= 0) return a / b;
     return -((-a) / b) - 1;
-} 
+}
 
-// long long ceil
-long long fracceil(long long a, long long b) {
+// return ceil(a / b)
+template <typename T, typename U, typename R = T>
+R fracceil(T a, U b) {
     assert(b != 0);
     if (a % b == 0) return a / b;
     if (a >= 0) return a / b + 1;
