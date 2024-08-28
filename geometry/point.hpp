c41d4221d46c77d55ae294a92cb3d76dfbc852cb
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-29 01:45:51 +0900

    愛ゆえの決断

--- geometry/point.hpp
+++ geometry/point.hpp
@@ -10,8 +10,14 @@ struct Point {
     bool operator<(const Point& p) const {
         return x != p.x ? x < p.x : y < p.y;
     }
+    bool operator<=(const Point& p) const {
+        return x != p.x ? x < p.x : y <= p.y;
+    }
     bool operator>(const Point& p) const {
         return x != p.x ? x > p.x : y > p.y;
+    }
+    bool operator>=(const Point& p) const {
+        return x != p.x ? x > p.x : y >= p.y;
     }
     bool operator==(const Point& p) const {
         return x == p.x && y == p.y;
