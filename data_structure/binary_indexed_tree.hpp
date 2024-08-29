3b489d69151b29cc824c097c63f0f290376da019
kk2 <104248260+kk2a@users.noreply.github.com>
2024-08-30 00:26:27 +0900

    add BIT

---
+++ data_structure/binary_indexed_tree.hpp
@@ -0,0 +1,39 @@
+#ifndef DATA_STRUCTURE_BINARY_INDEXED_TREE_HPP
+#define DATA_STRUCTURE_BINARY_INDEXED_TREE_HPP 1
+
+namespace kk2 {
+
+template <typename T>
+struct BinaryIndexedTree {
+    BinaryIndexedTree() : _n(0) {}
+    BinaryIndexedTree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        for (p++; p <= _n; p += p & -p) data[p - 1] += x;
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+    T get(int p) {
+        assert(0 <= p && p < _n);
+        return sum(p + 1) - sum(p);
+    }
+
+  private:
+    int _n;
+    std::vector<T> data;
+
+    T sum(int r) {
+        T s = 0;
+        for (; r > 0; r -= r & -r) s += data[r - 1];
+        return s;
+    }
+};
+
+} // namespace kk2
+
+#endif // #ifndef DATA_STRUCTURE_BINARY_INDEXED_TREE_HPP
