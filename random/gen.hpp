a25db68d32994c95c7a91532d34d3d9b19603651
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-02 06:43:43 +0900

    気が向いたら手を付けます

---
+++ random/gen.hpp
@@ -0,0 +1,16 @@
+#ifndef RANDOM_GEN_HPP
+#define RANDOM_GEN_HPP 1
+
+namespace kk2 {
+
+namespace random {
+
+struct Random {
+    
+};
+
+} // namespace random
+
+} // namespace kk2
+
+#endif // RANDOM_GEN_HPP
---
+++ random/graph.hpp
@@ -0,0 +1,15 @@
+#ifndef RANDOM_GRAPH_HPP
+#define RANDOM_GRAPH_HPP 1
+
+#include "gen.hpp"
+
+namespace kk2 {
+
+namespace random {
+
+
+} // namespace random
+
+} // namespace kk2
+
+#endif // RANDOM_GRAPH_HPP
