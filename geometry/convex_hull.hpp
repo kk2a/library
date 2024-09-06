3ed204fc76d38af4ae67759a866967ade1ec6c58
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-06 10:52:34 +0900

    add idx member

--- geometry/convex_hull.hpp
+++ geometry/convex_hull.hpp
@@ -8,6 +8,7 @@ namespace kk2 {
 template <typename T>
 struct ConvexHull {
     vector<Point<T>> ps, hull;
+    vector<int> idx;
 
     ConvexHull() = default;
     ConvexHull(const vector<Point<T>>& ps) : ps(ps) {}
@@ -25,28 +26,36 @@ struct ConvexHull {
             hull = ps;
             return;
         }
-        sort(begin(ps), end(ps));
-        vector<Point<T>> up, dw;
-        up.push_back(ps[0]);
-        dw.push_back(ps[0]);
-        up.push_back(ps[1]);
-        dw.push_back(ps[1]);
+        vc<pair<Point<T>, int>> tmp(_n);
+        rep (i, _n) tmp[i] = {ps[i], i};
+        sort(all(tmp));
+        vector<pair<Point<T>, int>> up, dw;
+        up.push_back(tmp[0]);
+        dw.push_back(tmp[0]);
+        up.push_back(tmp[1]);
+        dw.push_back(tmp[1]);
 
         for (int i = 2; i < _n; i++) {
-            while (size(up) >= 2 && cross(up[size(up) - 1]
-                   - up[size(up) - 2], ps[i] - up[size(up) - 1]) >= 0) {
+            while (size(up) >= 2 && cross(up[size(up) - 1].first
+                   - up[size(up) - 2].first, tmp[i].first - up[size(up) - 1].first) >= 0) {
                 up.pop_back();
             }
-            while (size(dw) >= 2 && cross(dw[size(dw) - 1]
-                   - dw[size(dw) - 2], ps[i] - dw[size(dw) - 1]) <= 0) {
+            while (size(dw) >= 2 && cross(dw[size(dw) - 1].first
+                   - dw[size(dw) - 2].first, tmp[i].first - dw[size(dw) - 1].first) <= 0) {
                 dw.pop_back();
             }
-            up.push_back(ps[i]);
-            dw.push_back(ps[i]);
+            up.push_back(tmp[i]); 
+            dw.push_back(tmp[i]);
         }
-        hull = up;
+        hull.reserve(size(up) + size(dw) - 2);
+        idx.resize(_n, -1);
+        for (int i = 0; i < (int)size(up); i++) {
+            hull.emplace_back(up[i].first);
+            idx[up[i].second] = i;
+        }
         for (int i = size(dw) - 2; i > 0; i--) {
-            hull.emplace_back(dw[i]);
+            hull.emplace_back(dw[i].first);
+            idx[dw[i].second] = size(hull) - 1;
         }
     }
 };
