6ab7961e456becd40b2494923af952ef59f0f2ac
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-06 16:31:23 +0900

    type traits

---
+++ type_traits/type_traits.hpp
@@ -0,0 +1,39 @@
+#ifndef TYPE_TRAITS_HPP
+#define TYPE_TRAITS_HPP 1
+
+namespace kk2 {
+
+template <typename T>
+using is_signed_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value or
+                              std::is_same<T, __int128>::value,
+                              std::true_type, std::false_type>::type;
+
+template <typename T>
+using is_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __uint128_t>::value or
+                              std::is_same<T, unsigned __int128>::value,
+                              std::true_type, std::false_type>::type;
+
+template <typename T>
+using is_integral_extended =
+    typename std::conditional<std::is_integral<T>::value or
+                              is_signed_int128<T>::value or
+                              is_unsigned_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+template <typename T>
+using is_signed_extended =
+    typename std::conditional<std::is_signed<T>::value or
+                              is_signed_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+template <typename T>
+using is_unsigned_extended =
+    typename std::conditional<std::is_unsigned<T>::value or
+                              is_unsigned_int128<T>::value,
+                              std::true_type, std::false_type>::type;
+
+} // namespace kk2
+
+#endif // TYPE_TRAITS_HPP
