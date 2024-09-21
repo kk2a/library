d0c2166b397220fcdc6fd877f7f298cf027cfd99
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-21 15:57:39 +0900

    random

--- random/gen.hpp
+++ random/gen.hpp
@@ -1,13 +1,63 @@
 #ifndef RANDOM_GEN_HPP
 #define RANDOM_GEN_HPP 1
 
+#include <algorithm>
+#include <cassert>
+#include <unordered_set>
+#include <vector>
+
+#include "seed.hpp"
+
 namespace kk2 {
 
 namespace random {
 
-struct Random {
-    
-};
+using i64 = long long;
+using u64 = unsigned long long;
+
+u64 xorshift128plus(u64 &x, u64 &y) {
+    u64 t = x;
+    t ^= t << 23;
+    t ^= t >> 17;
+    t ^= y ^ (y >> 26);
+    x = y;
+    y = t;
+    return x + y;
+}
+
+void warm_up(u64 &x, u64 &y) {
+    for (int i = 0; i < 10; i++) xorshift128plus(x, y);
+}
+
+u64 rng() {
+    static bool first = true;
+    static u64 x = seed(), y = seed();
+    if (first) {
+        warm_up(x, y);
+        first = false;
+    }
+    return xorshift128plus(x, y);
+}
+
+// [l, r)
+i64 rng(i64 l, i64 r) {
+    assert(l < r);
+    return l + rng() % (r - l);
+}
+
+// [l, r)
+std::vector<i64> distinct_rng(i64 l, i64 r, i64 n) {
+    assert(l < r and n <= r - l);
+    std::unordered_set<i64> st;
+    for (i64 i = n; i; --i) {
+        i64 m = rng(l, r + 1 - i);
+        if (st.find(m) != st.end()) m = r - i;
+        st.insert(m);
+    }
+    std::vector<i64> res(st.begin(), st.end());
+    std::sort(res.begin(), res.end());
+    return res;
+}
 
 } // namespace random
 
---
+++ random/seed.hpp
@@ -0,0 +1,39 @@
+#ifndef RANDOM_SEED_HPP
+#define RANDOM_SEED_HPP 1
+
+#include <chrono>
+
+namespace kk2 {
+
+namespace random {
+
+using u64 = unsigned long long;
+
+u64 non_deterministic_seed() {
+    u64 seed = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                   std::chrono::high_resolution_clock::now().time_since_epoch())
+                   .count();
+    seed ^= reinterpret_cast<u64>(&seed);
+    seed ^= seed << 5;
+    seed ^= seed >> 41;
+    seed ^= seed << 20;
+    return seed;
+}
+
+u64 deterministic_seed() {
+    return 5801799128519729247ull;
+}
+
+u64 seed() {
+#if defined(KK2) && !defined(KK2_RANDOM_NON_DETERMINISTIC)
+    return deterministic_seed();
+#else
+    return non_deterministic_seed();
+#endif
+}
+
+} // namespace random
+
+} // namespace kk2
+
+#endif // RANDOM_SEED_HPP
