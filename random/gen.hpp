a2b03db9f4a80fddcd860fdd6b7e748667d69c50
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-22 02:51:29 +0900

    added shuffle

--- random/gen.hpp
+++ random/gen.hpp
@@ -3,6 +3,7 @@
 
 #include <algorithm>
 #include <cassert>
+#include <numeric>
 #include <unordered_set>
 #include <vector>
 
@@ -26,6 +27,7 @@ u64 xorshift128plus(u64 &x, u64 &y) {
 }
 
 constexpr int iterations = 100;
+
 void warm_up(u64 &x, u64 &y) {
     for (int i = 0; i < iterations; i++) xorshift128plus(x, y);
 }
@@ -57,6 +59,32 @@ std::vector<i64> distinct_rng(i64 l, i64 r, i64 n) {
     }
     std::vector<i64> res(st.begin(), st.end());
     std::sort(res.begin(), res.end());
+    return res;
+}
+
+template <class Iter> void shuffle(Iter first, Iter last) {
+    if (first == last) return;
+    int len = 1;
+    for (auto it = first + 1; it != last; ++it) {
+        len++;
+        int j = rng(0, len);
+        if (j != len - 1) std::iter_swap(first + j, it);
+    }
+}
+
+template <class T> std::vector<T> perm(int n) {
+    std::vecotr<T> res(n);
+    std::iota(res.begin(), res.end(), T(0));
+    shuffle(res.begin(), res.end());
+    return res;
+}
+
+template <class T> std::vector<T> choices(int l, int r, int k) {
+    assert(l < r and k <= r - l);
+    std::vector<T> res(r - l);
+    std::iota(res.begin(), res.end(), T(l));
+    shuffle(res.begin(), res.end());
+    res.resize(k);
     return res;
 }
 
