d6618a6994fe12098d8856ebc7990992cd8d7a55
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-22 03:26:12 +0900

    random graph

--- random/graph.hpp
+++ random/graph.hpp
@@ -1,12 +1,49 @@
 #ifndef RANDOM_GRAPH_HPP
 #define RANDOM_GRAPH_HPP 1
 
+#include <type_traits>
+#include <vector>
+
 #include "gen.hpp"
+#include "../graph/graph.hpp"
 
 namespace kk2 {
 
 namespace random {
 
+template <bool is_weighted, bool is_directed, class T=int>
+using adjlist = std::conditional<is_weighted, WAdjList<T, is_directed>, AdjList<is_directed>>;
+
+template <bool is_weighted, bool is_directed, class T=int>
+void add_edge(adjlist<is_weighted, is_directed, T> &g, int from, int to, T l=0, T r=1e9) {
+    if constexpr (is_weighted) {
+        T w = rng(l, r);
+        g.add_edge(from, to, w);
+    } else {
+        g.add_edge(from, to);
+    }
+}
+
+template <bool is_weighted, bool is_directed, class T=int>
+adjlist<is_weighted, is_directed, T> Graph(int n, int m, T l=0, T r=1e9) {
+    adjlist<is_weighted, is_directed, T> g(n, false);
+    for (int i = 0; i < m; i++) {
+        int u = rng(0, n), v = rng(0, n);
+        add_edge<is_weighted, is_directed, T>(g, u, v, l, r);
+    }
+    return g;
+}
+
+template <bool is_weighted, class T=int>
+adjlist<is_weighted, false, T> Tree(int n, T l=0, T r=1e9) {
+    adjlist<is_weighted, false, T> g(n, false);
+    std::vector<int> p = perm<int>(n);
+    for (int i = 1; i < n; i++) {
+        int u = rng(0, i), v = i;
+        add_edge<is_weighted, false, T>(g, p[u], p[v], l, r);
+    }
+    return g;
+}
 
 } // namespace random
 
