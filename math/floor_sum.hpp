2f9c2c1f3874cfc4f11c484c981252fcb0ac4dd5
kk2 <104248260+kk2a@users.noreply.github.com>
2024-09-23 06:36:47 +0900

    sum of floor

---
+++ math/floor_sum.hpp
@@ -0,0 +1,29 @@
+#ifndef MATH_FLOOR_SUM_HPP
+#define MATH_FLOOR_SUM_HPP 1
+
+#include <algorithm>
+
+namespace kk2 {
+
+// https://atcoder.jp/contests/practice2/editorial/579
+// sum_{i=0}^{n-1} floor((a * i + b) / m)
+template <class T> T sum_of_floor(T n, T m, T a, T b) {
+    T res = 0;
+    while (true) {
+        if (a >= m) res += (n - 1) * n / 2 * (a / m), a %= m;
+        if (b >= m) res += n * (b / m), b %= m;
+        T max = a * n + b;
+        if (max < m) break;
+        n = max / m, b = max % m;
+        std::swap(a, m);
+    }
+    return res;
+}
+
+template <class T> T sum_of_floor(T n, T m, T a, T b, T start) {
+    return sum_of_floor(n - start, m, a, b + a * start);
+}
+
+} // namespace kk2
+
+#endif // MATH_FLOOR_SUM_HPP
