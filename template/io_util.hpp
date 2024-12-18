46642ba142055469cc9e3b07a16f15eabf2fd243
kk2 <104248260+kk2a@users.noreply.github.com>
2024-12-18 14:02:29 +0900

    io_util

--- template/io_util.hpp
+++ template/io_util.hpp
@@ -7,57 +7,74 @@
 
 #include "../type_traits/type_traits.hpp"
 
-template <class IStream, class T, class U, kk2::is_istream_t<IStream> *>
-IStream &operator>>(IStream &, std::pair<T, U> &);
+// なんかoj verifyはプロトタイプ宣言が落ちる
 
-template <class OStream, class T, class U, kk2::is_ostream_t<OStream> *>
-OStream &operator<<(OStream &, const std::pair<T, U> &);
+namespace impl {
 
-template <class IStream, class T, kk2::is_istream_t<IStream> *>
-IStream &operator>>(IStream &, std::vector<T> &);
+struct read {
+    template <class IStream, class T>
+    static void all_read(IStream &is, T &x) {
+        is >> x;
+    }
 
-template <class OStream, class T, kk2::is_ostream_t<OStream> *>
-OStream &operator<<(OStream &, const std::vector<T> &);
+    template <class IStream, class T, class U>
+    static void all_read(IStream &is, std::pair<T, U> &p) {
+        all_read(is, p.first);
+        all_read(is, p.second);
+    }
 
-template <class IStream, class T, size_t F, kk2::is_istream_t<IStream> *>
-IStream &operator>>(IStream &, std::array<T, F> &);
+    template <class IStream, class T>
+    static void all_read(IStream &is, std::vector<T> &v) {
+        for (T &x : v) all_read(is, x);
+    }
 
-template <class OStream, class T, size_t F, kk2::is_ostream_t<OStream> *>
-OStream &operator<<(OStream &, const std::array<T, F> &);
+    template <class IStream, class T, size_t F>
+    static void all_read(IStream &is, std::array<T, F> &a) {
+        for (T &x : a) all_read(is, x);
+    }
+};
 
-template <class IStream, class T, class U, kk2::is_istream_t<IStream> * = nullptr>
-IStream &operator>>(IStream &is, std::pair<T, U> &p) {
-    is >> p.first >> p.second;
-    return is;
-}
+struct write {
+    template <class OStream, class T>
+    static void all_write(OStream &os, const T &x) {
+        os << x;
+    }
 
-template <class OStream, class T, class U, kk2::is_ostream_t<OStream> * = nullptr>
-OStream &operator<<(OStream &os, const std::pair<T, U> &p) {
-    os << p.first << ' ' << p.second;
-    return os;
-}
+    template <class OStream, class T, class U>
+    static void all_write(OStream &os, const std::pair<T, U> &p) {
+        all_write(os, p.first);
+        all_write(os, ' ');
+        all_write(os, p.second);
+    }
+
+    template <class OStream, class T>
+    static void all_write(OStream &os, const std::vector<T> &v) {
+        for (int i = 0; i < (int)v.size(); ++i) {
+            if (i) all_write(os, ' ');
+            all_write(os, v[i]);
+        }
+    }
+
+    template <class OStream, class T, size_t F>
+    static void all_write(OStream &os, const std::array<T, F> &a) {
+        for (int i = 0; i < (int)F; ++i) {
+            if (i) all_write(os, ' ');
+            all_write(os, a[i]);
+        }
+    }
+};
+
+} // namespace impl
 
 template <class IStream, class T, kk2::is_istream_t<IStream> * = nullptr>
-IStream &operator>>(IStream &is, std::vector<T> &v) {
-    for (T &x : v) is >> x;
+IStream &operator>>(IStream &is, T &x) {
+    impl::read::all_read(is, x);
     return is;
 }
 
 template <class OStream, class T, kk2::is_ostream_t<OStream> * = nullptr>
-OStream &operator<<(OStream &os, const std::vector<T> &v) {
-    for (int i = 0; i < (int)v.size(); i++) { os << v[i] << (i + 1 == (int)v.size() ? "" : " "); }
-    return os;
-}
-
-template <class IStream, class T, size_t F, kk2::is_istream_t<IStream> * = nullptr>
-IStream &operator>>(IStream &is, std::array<T, F> &a) {
-    for (auto &x : a) is >> x;
-    return is;
-}
-
-template <class OStream, class T, size_t F, kk2::is_ostream_t<OStream> * = nullptr>
-OStream &operator<<(OStream &os, const std::array<T, F> &a) {
-    for (int i = 0; i < (int)F; i++) { os << a[i] << (i + 1 == (int)F ? "" : " "); }
+OStream &operator<<(OStream &os, const T &x) {
+    impl::write::all_write(os, x);
     return os;
 }
 
