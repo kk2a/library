6618daabdfc79d2a769f241bd523ba42e4e34d3c
kk2 <104248260+kk2a@users.noreply.github.com>
2024-12-21 14:29:01 +0900

    interval heap

---
+++ data_structure/interval_heap.hpp
@@ -0,0 +1,32 @@
+#ifndef DATA_STRUCTURE_INTERVAL_HEAP_HPP
+#define DATA_STRUCTURE_INTERVAL_HEAP_HPP 1
+
+#include <algorithm>
+#include <cassert>
+#include <functional>
+#include <vector>
+
+namespace kk2 {
+
+template <class T, class Compare = std::less<T>>
+struct IntervalHeap {
+    std::vector<T> v;
+
+    IntervalHeap() = default;
+    IntervalHeap(const std::vector<T> &a) : v(a) {
+        make_heap();
+    }
+
+    void make_heap() {
+        
+    }
+
+  private:
+    static bool op(T x, T y) {
+        return Compare{}(x, y);
+    }
+};
+
+} // namespace kk2
+
+#endif // DATA_STRUCTURE_INTERVAL_HEAP_HPP
