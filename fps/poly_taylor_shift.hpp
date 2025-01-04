ebf50bca3a944a55bb71c2727f069894d99577db
kk2 <104248260+kk2a@users.noreply.github.com>
2025-01-05 07:33:32 +0900

    extend

--- fps/poly_taylor_shift.hpp
+++ fps/poly_taylor_shift.hpp
@@ -10,6 +10,7 @@ namespace kk2 {
 template <class FPS, class mint = typename FPS::value_type> FPS taylor_shift(const FPS &f_, mint a) {
     FPS f(f_);
     int n = f.size();
+    Comb<mint>::extend(n);
     for (int i = 0; i < n; i++) f[i] *= Comb<mint>::fact(i);
     f.inplace_rev();
     FPS g(n, mint(1));
