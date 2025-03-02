2311756ece6104826c8ddcdab81c86d9af26d26d
kk2 <104248260+kk2a@users.noreply.github.com>
2025-03-02 21:18:36 +0900

    large_fact_arb_mod.test.cppにcomb_large.hppとcomb.hppのインクルードを追加

--- verify/unit_test/large_fact_arb_mod.test.cpp
+++ verify/unit_test/large_fact_arb_mod.test.cpp
@@ -1,6 +1,8 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb" 
 
 #include "../../modint/modint.hpp"
+#include "../../math_mod/comb_large.hpp"
+#include "../../math_mod/comb.hpp"
 #include "../../random/gen.hpp"
 #include "../../template/template.hpp"
 using namespace std;
