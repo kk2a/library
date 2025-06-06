7d427b13021463e6646d4d0beeffaabf6eb79baf
kk2 <104248260+kk2a@users.noreply.github.com>
2025-06-06 16:35:52 +0900

    feat(container_traits): add traits for various standard container types

--- type_traits/container_traits.hpp
+++ type_traits/container_traits.hpp
@@ -5,11 +5,38 @@
 
 #include <type_traits>
 #include <vector>
+#include <string>
+#include <array>
+#include <deque>
+#include <list>
 
 namespace kk2 {
 
 template <typename T> struct is_vector : std::false_type {};
 template <typename T, typename Alloc> struct is_vector<std::vector<T, Alloc>> : std::true_type {};
+
+// コンテナかどうかを判定するtraits
+template <typename T> struct is_container : std::false_type {};
+
+// 基本的なコンテナ型の特殊化
+template <typename T, typename Alloc> 
+struct is_container<std::vector<T, Alloc>> : std::true_type {};
+
+template <typename CharT, typename Traits, typename Alloc>
+struct is_container<std::basic_string<CharT, Traits, Alloc>> : std::true_type {};
+
+template <typename T, std::size_t N>
+struct is_container<std::array<T, N>> : std::true_type {};
+
+template <typename T, typename Alloc>
+struct is_container<std::deque<T, Alloc>> : std::true_type {};
+
+template <typename T, typename Alloc>
+struct is_container<std::list<T, Alloc>> : std::true_type {};
+
+// SFINAEでコンテナを判定するためのヘルパー
+template <typename T>
+using is_container_t = typename std::enable_if_t<is_container<T>::value, std::nullptr_t>;
 
 } // namespace kk2
 
