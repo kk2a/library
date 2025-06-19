776aa298c991daf72af699573fb2f2c9d55ade50
kk2 <104248260+kk2a@users.noreply.github.com>
2025-06-19 11:03:22 +0900

    fix(verify): update include for static rolling hash and adjust usage in test

--- verify/yosupo_string/string_z_roliha.test.cpp
+++ verify/yosupo_string/string_z_roliha.test.cpp
@@ -1,17 +1,17 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm" 
 
-#include "../../string/rolling_hash.hpp"
+#include "../../string/static_rolling_hash.hpp"
 #include "../../template/template.hpp"
 using namespace std;
 
 int main() {
     string s;
     kin >> s;
-    kk2::Roliha rs(s);
+    kk2::SRoliha rs(s);
 
     int n = s.size();
     vc<int> z(n);
-    rep (i, n) z[i] = kk2::Roliha::lcp(rs, 0, n, rs, i, n);
+    rep (i, n) z[i] = kk2::SRoliha::lcp(rs, 0, n, rs, i, n);
     kout << z << kendl;
 
     return 0;
